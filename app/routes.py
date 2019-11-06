b87ea8327e2156cb6fab760a03bc1621021fceac
legianni <legianni@gmail.com>
2019-11-05 19:49:57 -0800

    Added client_secret.json and eventSync route

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,12 @@ from app.models import Friend
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
-from flask import request
+from flask import request, Response
 from werkzeug.urls import url_parse
+from apiclient import discovery
+from oauth2client import client
+from googleapiclient import sample_tools
+import sys,json,flask,httplib2,uuid
 
 @app.route('/')
 def landingPage():
@@ -157,6 +161,36 @@ def createEvent():
         flash('Congratulations, you have created an event!')
         return redirect(url_for('index'))
     return render_template('createEvent.html', title='Create Event', form=form)
+
+#google api authorization
+@app.route('/event/sync')
+def syncEvent():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+    if credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    return flask.render_template('index.html')
+
+#Begin oauth callback route
+@app.route('/oauth2callback')
+def oauth2callback():
+  flow = client.flow_from_clientsecrets(
+      'client_secrets.json',
+      scope='https://www.googleapis.com/auth/calendar',
+      redirect_uri=flask.url_for('oauth2callback', _external=True))
+  if 'code' not in flask.request.args:
+    auth_uri = flow.step1_get_authorize_url()
+    return flask.redirect(auth_uri)
+  else:
+    auth_code = flask.request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    flask.session['credentials'] = credentials.to_json()
+    return flask.redirect(flask.url_for('index'))
+
 
 # route to display the schedule. users will be able to update their schedule here
 @app.route('/schedule/create', methods=['GET', 'POST'])
