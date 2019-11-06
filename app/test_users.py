170cbf31d3bbab63c22bcb5e383401cb769cbddc
legianni <legianni@gmail.com>
2019-11-06 14:04:39 -0800

    added unit tests for users and create event

--- app/test_users.py
+++ app/test_users.py
@@ -10,3 +10,9 @@ def test_new_user(new_user):
     assert new_user.username == 'TESTING123'
     assert new_user.email == 'TESTING123@gmail.com'
     assert new_user.password_hash != 'TESTING'
+
+def test_user_id(new_user):
+    new_user.id=17
+    assert isinstance(new_user.get_id(),str)
+    assert not isinstance(new_user.get_id(),int)
+    assert new_user.get_id() == "17"
